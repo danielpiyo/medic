ba63621950407d0424bbe2dfff1ad41b24f61f43
danielpiyo <danielopiyo8@gmail.com>
2023-09-04 16:12:25 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mclinic-nurse-app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
